579d7c4a7d57fac365d18ca5fb43e4babec21ff6
slynerds <53099318+slynerds@users.noreply.github.com>
2019-12-03 00:30:55 +0530

    Updates

---
+++ precache-manifest.d578afb446ceb36826cc5bd47f981ce1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a3df23c21a1c182e728a7b579df0f0",
+    "url": "/wordpress-app/index.html"
+  },
+  {
+    "revision": "853c730f4fe4ae86c800",
+    "url": "/wordpress-app/static/css/2.247797c6.chunk.css"
+  },
+  {
+    "revision": "3bc6215063dbcaff7f6c",
+    "url": "/wordpress-app/static/css/main.5f490d5b.chunk.css"
+  },
+  {
+    "revision": "853c730f4fe4ae86c800",
+    "url": "/wordpress-app/static/js/2.9e980914.chunk.js"
+  },
+  {
+    "revision": "3bc6215063dbcaff7f6c",
+    "url": "/wordpress-app/static/js/main.fe11bd5b.chunk.js"
+  },
+  {
+    "revision": "9c90a0ac39d38f963713",
+    "url": "/wordpress-app/static/js/runtime-main.dd1f1511.js"
+  }
+]);
