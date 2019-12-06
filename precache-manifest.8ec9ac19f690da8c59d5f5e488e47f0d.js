c29b7e49c1f6d837dc4e65f80e8f8b8edda84a0f
keshavk2910 <keshavk2910@gmail.com>
2019-12-06 08:28:21 +0530

    Updates

---
+++ precache-manifest.8ec9ac19f690da8c59d5f5e488e47f0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe5b275bc4664fbca6377f72fe7002a0",
+    "url": "/wordpress-app/index.html"
+  },
+  {
+    "revision": "869e8343b34b2b4014b7",
+    "url": "/wordpress-app/static/css/2.247797c6.chunk.css"
+  },
+  {
+    "revision": "8adafde14e1ce89b6899",
+    "url": "/wordpress-app/static/css/main.3fb954d7.chunk.css"
+  },
+  {
+    "revision": "869e8343b34b2b4014b7",
+    "url": "/wordpress-app/static/js/2.29693d02.chunk.js"
+  },
+  {
+    "revision": "8adafde14e1ce89b6899",
+    "url": "/wordpress-app/static/js/main.faabc16e.chunk.js"
+  },
+  {
+    "revision": "9c90a0ac39d38f963713",
+    "url": "/wordpress-app/static/js/runtime-main.dd1f1511.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wordpress-app/precache-manifest.90009a61701de2077ca6c7e2ab555fb3.js"
+  "/wordpress-app/precache-manifest.8ec9ac19f690da8c59d5f5e488e47f0d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wordpress-app/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
